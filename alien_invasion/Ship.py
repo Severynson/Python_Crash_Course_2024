20339dee64dfce20675c0b83bbe6b7a0a3556e0b
Severyn Kurach <severynson.kurach@gmail.com>
2024-06-02 16:45:55 +0200

    alien_invasion_completed

--- alien_invasion/Ship.py
+++ alien_invasion/Ship.py
@@ -1,15 +1,18 @@
 import pygame
+from pygame.sprite import Sprite
 
 UA_SPACESHIP_SIZE = (220, 240)
 UPA_SPACESHIP_SIZE = (150, 240)
 DEFEATED_SPACESHIP_SIZE = (220, 200)
 GRAVE_SIZE = (200, 200)
 
-class Ship:
+
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -44,23 +47,25 @@ class Ship:
         self.x = float(self.rect.x)
 
     def update_skin(self, ships_left=3):
-            "Updates the ship's skin depending on how many ships left."
-            if ships_left >= 3:
-                self.image = pygame.image.load("alien_invasion/images/ua_spaceship.bmp")
-                self.image = pygame.transform.scale(self.image, UA_SPACESHIP_SIZE)
-            elif ships_left == 2:
-                self.image = pygame.image.load("alien_invasion/images/upa_spaceship.bmp")
-                self.image = pygame.transform.scale(self.image, UPA_SPACESHIP_SIZE)
-            elif ships_left == 1:
-                self.image = pygame.image.load("alien_invasion/images/defeated_spaceship.bmp")
-                self.image = pygame.transform.scale(self.image, DEFEATED_SPACESHIP_SIZE)
-            else:
-                self.image = pygame.image.load("alien_invasion/images/grave.bmp")
-                self.image = pygame.transform.scale(self.image, GRAVE_SIZE)
-            
-            # Update the rect attribute to match the new size of the image
-            self.rect = self.image.get_rect()
-            
-            # Calculate the center position based on the screen size
-            screen_rect = self.screen.get_rect()
-            self.rect.center = screen_rect.center
+        "Updates the ship's skin depending on how many ships left."
+        if ships_left >= 3:
+            self.image = pygame.image.load("alien_invasion/images/ua_spaceship.bmp")
+            self.image = pygame.transform.scale(self.image, UA_SPACESHIP_SIZE)
+        elif ships_left == 2:
+            self.image = pygame.image.load("alien_invasion/images/upa_spaceship.bmp")
+            self.image = pygame.transform.scale(self.image, UPA_SPACESHIP_SIZE)
+        elif ships_left == 1:
+            self.image = pygame.image.load(
+                "alien_invasion/images/defeated_spaceship.bmp"
+            )
+            self.image = pygame.transform.scale(self.image, DEFEATED_SPACESHIP_SIZE)
+        else:
+            self.image = pygame.image.load("alien_invasion/images/grave.bmp")
+            self.image = pygame.transform.scale(self.image, GRAVE_SIZE)
+
+        # Update the rect attribute to match the new size of the image
+        self.rect = self.image.get_rect()
+
+        # Calculate the center position based on the screen size
+        screen_rect = self.screen.get_rect()
+        self.rect.center = screen_rect.center
